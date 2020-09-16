0cf79bdb0718094e0431a61623b93881115aa1fa
Brian Levin <bml201095@gmail.com>
2020-09-16 01:12:17 -0400

    added constants values to the morgage calculator

--- Main.java
+++ Main.java
@@ -2,7 +2,8 @@
 public class Main {  // main object which will hold all the methods
 
     public static void main(String[] args) { // Public= Global Access, Static= invoked without isntating class to save memory, Void= Doesnt return anything once program terminates , main: main method starting point that JVM looks for, String args, stores java command line arguments as strings and args are not fixed and can use an name in place of it.
-
+        final byte MONTHS_IN_YEAR = 12; // byte= 8 bit storgae  final= constant
+        final byte PERCENT = 100;
 
 
     }
