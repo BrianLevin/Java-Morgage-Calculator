07b671cfbf3ff26d9fa1570fff22bdf076171fde
Brian Levin <bml201095@gmail.com>
2020-09-16 00:59:15 -0400

    added public static void mainString args as the main method throughout the document

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
 
-public class Main {
+public class Main {  // main object which will hold all the methods
+
+    public static void main(String[] args) { // Public= Global Access, Static= invoked without isntating class to save memory, Void= Doesnt return anything once program terminates , main: main method starting point that JVM looks for, String args, stores java command line arguments as strings and args are not fixed and can use an name in place of it.
 
 
+
+    }
 
 }
